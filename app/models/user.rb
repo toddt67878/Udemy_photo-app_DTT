c9699ed52f2d76c03f03272f8a06b7e65f09fc14
Todd Traylor <toddt67878@yahoo.com>
2020-01-16 11:10:34 -0600

    Payment Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          :confirmable
+  has_one :payment
+  accepts_nested_attributes_for :payment
 end
